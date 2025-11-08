d09bd7bc0ec0dddb5cd9a1969566637c76d413fa
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-11-08 16:15:19 +0545

    Add product API script to fetch and display product prices

---
+++ Python Advanced/api_work/product_api.py
@@ -0,0 +1,12 @@
+import requests
+
+api_url = "https://690f18d6bd0fefc30a06b965.mockapi.io/product/"
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    products = response.json()
+    for product in products:
+        print(f"The price of {product["product"]} is {product["price"]}.")
+else:
+    print("Failed to retrieve products.")
