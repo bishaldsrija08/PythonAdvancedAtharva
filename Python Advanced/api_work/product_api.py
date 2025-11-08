725c1e8e8bccb6364bf8044fc5ef7d26a9a15201
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-11-08 16:31:33 +0545

    Fix header comment formatting and add product details retrieval functionality

--- Python Advanced/api_work/product_api.py
+++ Python Advanced/api_work/product_api.py
@@ -1,4 +1,4 @@
-# API to fetch product data and display prices
+# # API to fetch product data and display prices
 import requests
 
 api_url = "https://690f18d6bd0fefc30a06b965.mockapi.io/product/"
@@ -11,3 +11,14 @@ if response.status_code == 200:
         print(f"The price of {product["product"]} is {product["price"]}.")
 else:
     print("Failed to retrieve products.")
+    
+api_url2 = "https://690f18d6bd0fefc30a06b965.mockapi.io/product/1"
+response = requests.get(api_url2)
+
+if response.status_code == 200:
+    product = response.json()
+    print(f"Product ID: {product['id']}")
+    print(f"Product Name: {product['product']}")
+    print(f"Product Price: {product['price']}")
+else:
+    print("Failed to retrieve product.")
